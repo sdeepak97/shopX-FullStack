8e77f93dfb49b60b9ce4a0b2011ecb1378b3a233
Deepak S <sdeepak1325@gmail.com>
2021-07-10 00:28:18 +0530

    added DB config

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -4,15 +4,21 @@ import { CartEntity } from "./database/cart/entity/cart.entity";
 import { ProductEntity } from "./database/products/entity/products.entity";
 import { UserEntity } from "./database/user/entity/user.entity";
 import { UserInfoEntity } from "./database/user_info/entity/userinfo.entity";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectionOptions: ConnectionOptions = {
+
+  url: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
   type: "postgres",
-  host: "localhost",
+  host: process.env.Host || "localhost",
   port: 5432,
-  username: "postgres",
-  password: "9848755758",
-  database: "postgres",
-  entities: [UserEntity,CartEntity,UserInfoEntity,ProductEntity],
+  username: process.env.User || "postgres",
+  password: process.env.DB_Password || "9848755758",
+  database: process.env.Database || "postgres",
+  entities: [UserEntity, CartEntity, UserInfoEntity, ProductEntity],
   synchronize: true,
   dropSchema: false,
   migrationsRun: true,
